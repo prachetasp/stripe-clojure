4430ffd707807c22218c672bc654910d9735229a
Prachetas Prabhu <prachetas.github@gmail.com>
2014-11-19 06:54:39 +0000

    Set version to 1.0.0 and add scm for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject prachetasp/stripe-clojure "1.0.0-SNAPSHOT"
+(defproject prachetasp/stripe-clojure "1.0.0"
   :description "Clojure Stripe Library"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/prachetasp/stripe-clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 		 [clj-http "1.0.1"]])
